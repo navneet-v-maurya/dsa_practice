7425dba93387c391b4215d0693657cf70ef1a5d1
navneet <navneet.v.maurya@gmail.com>
2024-05-21 23:52:05 +0530

    done with sieve of eratosthenes algo

--- Algorithms/Maths.js
+++ Algorithms/Maths.js
@@ -18,4 +18,25 @@ const euclids = (num1, num2) => {
   return dividend;
 };
 
+//sieve of eratosthenes
+const sieve = (num) => {
+  const is_prime_arr = new Array(num + 1);
+  is_prime_arr.fill(true, 0, num + 1);
+  for (let i = 2; i * i <= is_prime_arr.length; i++) {
+    if (is_prime_arr[i]) {
+      for (let j = 2 * i; j < is_prime_arr.length; j = j + i) {
+        is_prime_arr[j] = false;
+      }
+    }
+  }
+  const arr = [];
+  for (let i = 2; i < is_prime_arr.length; i++) {
+    if (is_prime_arr[i]) {
+      arr.push(i);
+    }
+  }
+  return arr;
+};
+
 console.log(euclids(1005, 105));
+console.log(sieve(97));
