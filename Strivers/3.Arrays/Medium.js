266cf8d12e82665fec1f346f90a54e90e4617fd4
navneet <navneet.v.maurya@gmail.com>
2024-10-28 23:40:12 +0530

    Array Medium Day 3

--- Strivers/3.Arrays/Medium.js
+++ Strivers/3.Arrays/Medium.js
@@ -90,3 +90,74 @@ const sort_colors = (arr) => {
 };
 
 console.log(sort_colors([2, 0, 2, 1, 1, 0]));
+
+//Problem Statement: Given an integer array arr, find the contiguous subarray (containing at least one number) which
+//has the largest sum and returns its sum and prints the subarray.
+
+// Example 1:
+// Input:
+//  arr = [-2,1,-3,4,-1,2,1,-5,4]
+
+// Output:
+//  6
+
+// Explanation:
+//  [4,-1,2,1] has the largest sum = 6.
+
+// Examples 2:
+// Input:
+//  arr = [1]
+
+// Output:
+//  1
+
+// Explanation:
+//  Array has only one element and which is giving positive sum of 1.
+const kadanes_algorithm = (arr) => {};
+
+//Problem Statement: Given an array of N integers, write a program to return an element that occurs more than N/2 times in the
+//given array. You may consider that such an element always exists in the array.
+
+// Example 1:
+// Input Format
+// : N = 3, nums[] = {3,2,3}
+// Result
+// : 3
+// Explanation
+// : When we just count the occurrences of each number and compare with half of the size of the array, you will get 3 for the above solution.
+
+// Example 2:
+// Input Format:
+//   N = 7, nums[] = {2,2,1,1,1,2,2}
+
+// Result
+// : 2
+
+// Explanation
+// : After counting the number of times each element appears and comparing it with half of array size, we get 2 as result.
+
+// Example 3:
+// Input Format:
+//   N = 10, nums[] = {4,4,2,4,3,4,4,3,2,4}
+
+// Result
+// : 4
+
+const majority_element = (arr) => {
+  let count = 1;
+  let num = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (count === 0) {
+      num = arr[i];
+      count++;
+    } else if (num == arr[i]) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+  return num;
+};
+
+console.log(majority_element([2, 2, 1, 1, 1, 2, 2]));
