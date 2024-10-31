0b809af8068ef9c1732ffd68b15f1205751577db
navneet <navneet.v.maurya@gmail.com>
2024-11-01 03:14:28 +0530

    Array Medium Day 5

--- Strivers/3.Arrays/Medium.js
+++ Strivers/3.Arrays/Medium.js
@@ -230,3 +230,58 @@ const stock = (arr) => {
 };
 
 console.log(stock([7, 1, 5, 3, 6, 4]));
+
+//Problem Statement: There’s an array ‘A’ of size ‘N’ with an equal number of positive and negative elements. Without altering the relative
+//order of positive and negative elements, you must return an array of alternately positive and negative values.
+
+// Example 1:
+
+// Input:
+// arr[] = {1,2,-4,-5}, N = 4
+// Output:
+// 1 -4 2 -5
+
+// Explanation:
+
+// Positive elements = 1,2
+// Negative elements = -4,-5
+// To maintain relative ordering, 1 must occur before 2, and -4 must occur before -5.
+
+// Example 2:
+// Input:
+// arr[] = {1,2,-3,-1,-2,-3}, N = 6
+// Output:
+// 1 -3 2 -1 3 -2
+// Explanation:
+
+// Positive elements = 1,2,3
+// Negative elements = -3,-1,-2
+// To maintain relative ordering, 1 must occur before 2, and 2 must occur before 3.
+// Also, -3 should come before -1, and -1 should come before -2.
+
+const rearrange_array = (arr) => {
+  let pos, neg;
+  if (arr[0] < 0) {
+    neg = 0;
+    pos = 1;
+  } else {
+    pos = 0;
+    neg = 1;
+  }
+
+  const new_arr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 0) {
+      new_arr[pos] = arr[i];
+      pos += 2;
+    } else if (arr[i] < 0) {
+      new_arr[neg] = arr[i];
+      neg += 2;
+    }
+  }
+
+  return new_arr;
+};
+
+console.log(rearrange_array([3, 1, -2, -5, 2, -4]));
