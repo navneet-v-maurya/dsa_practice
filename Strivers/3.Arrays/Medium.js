f7d3422d3c43c5a026f6fb7b2c3f9bbeeaa24fa1
navneet <navneet.v.maurya@gmail.com>
2024-11-03 00:01:37 +0530

    Array Medium Day 7

--- Strivers/3.Arrays/Medium.js
+++ Strivers/3.Arrays/Medium.js
@@ -440,3 +440,88 @@ console.log(
     [1, 1, 1],
   ])
 );
+
+//Problem Statement: Given a matrix, your task is to rotate the matrix 90 degrees clockwise.
+
+// Example 1:
+// Input:
+//  [[1,2,3],[4,5,6],[7,8,9]]
+
+// Output
+// : [[7,4,1],[8,5,2],[9,6,3]]
+
+// Explanation:
+//  Rotate the matrix simply by 90 degree clockwise and return the matrix.
+
+// Example 2:
+// Input:
+//  [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+
+// Output:
+// [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+// Explanation:
+//  Rotate the matrix simply by 90 degree clockwise and return the matrix
+
+const transpose = (matrix) => {
+  //changing rows to column and column to rows
+  let temp;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = i + 1; j < matrix[i].length; j++) {
+      temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+
+  console.log("transpose", matrix);
+};
+
+const rotate = (matrix) => {
+  transpose(matrix);
+
+  let temp;
+  let start, end;
+
+  for (let i = 0; i < matrix.length; i++) {
+    start = 0;
+    end = matrix[i].length - 1;
+    while (start < end) {
+      temp = matrix[i][start];
+      matrix[i][start] = matrix[i][end];
+      matrix[i][end] = temp;
+      start++;
+      end--;
+    }
+  }
+  return matrix;
+};
+
+console.log(
+  rotate([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+);
+
+//Problem Statement: Given a Matrix, print the given matrix in spiral order.
+
+// Example 1:
+// Input: Matrix[][] = { { 1, 2, 3, 4 },
+// 		      { 5, 6, 7, 8 },
+// 		      { 9, 10, 11, 12 },
+// 	              { 13, 14, 15, 16 } }
+
+// Outhput: 1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10.
+// Explanation: The output of matrix in spiral form.
+
+// Example 2:
+// Input: Matrix[][] = { { 1, 2, 3 },
+// 	              { 4, 5, 6 },
+// 		      { 7, 8, 9 } }
+
+// Output: 1, 2, 3, 6, 9, 8, 7, 4, 5.
+// Explanation: The output of matrix in spiral form.
+
+const spiral_order = (matrix) => {};
