98b4ce6a4ab28468b209c291f86efe8adc84a331
navneet <navneet.v.maurya@gmail.com>
2024-11-09 01:01:43 +0530

    BS 1D Array Day 4

--- Strivers/4.Binary_Search/Bs_1D_Array.js
+++ Strivers/4.Binary_Search/Bs_1D_Array.js
@@ -285,3 +285,74 @@ const find_min_rotated = (arr) => {
 };
 
 console.log(find_min_rotated([3, 5, 1]));
+
+//Problem Statement: Given an array of length N. Peak element is defined as the element greater than both of its neighbors.
+//Formally, if 'arr[i]' is the peak element, 'arr[i - 1]' < 'arr[i]' and 'arr[i + 1]' < 'arr[i]'. Find the index(0-based) of
+//a peak element in the array. If there are multiple peak numbers, return the index of any peak number.
+
+// Example 1:
+// Input Format: arr[] = {1,2,3,4,5,6,7,8,5,1}
+// Result: 7
+// Explanation: In this example, there is only 1 peak that is at index 7.
+
+// Example 2:
+// Input Format: arr[] = {1,2,1,3,5,6,4}
+// Result: 1
+// Explanation: In this example, there are 2 peak numbers that are at indices 1 and 5. We can consider any of them.
+
+// Example 3:
+// Input Format: arr[] = {1, 2, 3, 4, 5}
+// Result: 4
+// Explanation: In this example, there is only 1 peak that is at the index 4.
+
+// Example 4:
+// Input Format: arr[] = {5, 4, 3, 2, 1}
+// Result: 0
+// Explanation: In this example, there is only 1 peak that is at the index 0.
+
+const peak_element = (arr) => {};
+
+//Problem Statement: Given an array of N integers. Every number in the array except one appears twice. Find the single number in the array.
+
+// Example 1:
+// Input Format:
+//  arr[] = {1,1,2,2,3,3,4,5,5,6,6}
+// Result:
+//  4
+// Explanation:
+//  Only the number 4 appears once in the array.
+
+// Example 2:
+// Input Format:
+//  arr[] = {1,1,3,5,5}
+// Result:
+//  3
+// Explanation:
+//  Only the number 3 appears once in the array.
+
+const single_non_duplicate = (arr) => {
+  let start = 0;
+  let end = arr.length - 1;
+  let mid;
+
+  if (arr[0] !== arr[1]) return arr[0];
+  if (arr[end] !== arr[end - 1]) return arr[end];
+
+  while (start <= end) {
+    mid = Math.floor((start + end) / 2);
+    if (arr[mid] !== arr[mid + 1] && arr[mid] !== arr[mid - 1]) {
+      return arr[mid];
+    }
+
+    if (
+      (mid % 2 === 1 && arr[mid] === arr[mid - 1]) ||
+      (mid % 2 === 0 && arr[mid] === arr[mid + 1])
+    ) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+};
+
+console.log(single_non_duplicate([3, 3, 7, 7, 10, 11, 11]));
