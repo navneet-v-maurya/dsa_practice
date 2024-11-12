eadd6f2d7f4ba1d9958ea78150eb14f7ad7b2250
navneet <navneet.v.maurya@gmail.com>
2024-11-13 01:00:57 +0530

    BS On Answers Day 2

--- Strivers/4.Binary_Search/Bs_On_Answers.js
+++ Strivers/4.Binary_Search/Bs_On_Answers.js
@@ -53,3 +53,63 @@ const min_eating_speed = (piles, hours) => {
 };
 
 console.log(min_eating_speed([30, 11, 23, 4, 20], 5));
+
+//Problem Statement: You are given an array of integers 'arr' and an integer i.e. a threshold value 'limit'.
+//Your task is to find the smallest positive integer divisor, such that upon dividing all the elements of the given array by it,
+//the sum of the division's result is less than or equal to the given threshold value.
+
+// Example 1:
+// Input Format:
+//  N = 5, arr[] = {1,2,3,4,5}, limit = 8
+// Result:
+//  3
+// Explanation:
+//  We can get a sum of 15(1 + 2 + 3 + 4 + 5) if we choose 1 as a divisor.
+// The sum is 9(1 + 1 + 2 + 2 + 3)  if we choose 2 as a divisor. Upon dividing all the elements of the array by 3, we get 1,1,1,2,2 respectively. Now, their sum is equal to 7 <= 8 i.e. the threshold value. So, 3 is the minimum possible answer.
+
+// Example 2:
+// Input Format:
+//  N = 4, arr[] = {8,4,2,3}, limit = 10
+// Result:
+//  2
+// Explanation:
+//  If we choose 1, we get 17 as the sum. If we choose 2, we get 9(4+2+1+2) <= 10 as the answer. So, 2 is the answer.
+
+const smallest_divisor = (arr, threshold) => {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  const is_below_threshold = (arr, val) => {
+    let count = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+      count += Math.ceil(arr[i] / val);
+    }
+
+    if (count <= threshold) return true;
+    return false;
+  };
+
+  let start = 1;
+  let mid;
+  let min = max;
+  while (start <= max) {
+    mid = Math.floor((start + max) / 2);
+    if (is_below_threshold(arr, mid)) {
+      if (mid < min) {
+        min = mid;
+      }
+      max = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return min;
+};
+
+console.log(smallest_divisor([44, 22, 33, 11, 1], 5));
