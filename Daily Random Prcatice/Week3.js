6812b0c962f969787f6d8a434dc323a676fa2e76
Navneet Maurya <navneet.maurya@softsensor.ai>
2025-11-05 17:00:16 +0530

    week 3 day 3

--- Daily Random Prcatice/Week3.js
+++ Daily Random Prcatice/Week3.js
@@ -1,6 +1,7 @@
 //WEEK 3
 
 //Day 1
+//1. Daily Temperatues
 const daily_temperatures = (temperatures) => {
     const result = new Array(temperatures.length).fill(0);
 
@@ -26,8 +27,7 @@ const daily_temperatures = (temperatures) => {
 console.log("daily_temperatures => ", daily_temperatures([73, 74, 75, 71, 69, 72, 76, 73]));
 
 //Day 2
-//Binary Tree Inorder Traversal
-
+//1. Binary Tree Inorder Traversal
 const inorder_traversal = (root) => {
     const result = [];
     const recursion = (node) => {
@@ -95,6 +95,7 @@ const tree = {
 
 console.log("inorder_traversal => ", inorder_traversal(tree));
 
+//2. Combinations
 const cominations = (candidates, target) => {
     const result = [];
 
@@ -120,3 +121,87 @@ const cominations = (candidates, target) => {
 };
 
 console.log("cominations => ", cominations([2, 3, 6, 7], 7));
+
+//Day 3
+//1. Min Depth of Tree
+const min_depth_of_tree = (root) => {
+    if (!root) return 0;
+
+    let min = Infinity;
+
+    const recursion = (node, count) => {
+        if (!node.left && !node.right) {
+            min = Math.min(min, count);
+            return;
+        }
+
+        if (node.left) {
+            recursion(node.left, count + 1);
+        }
+
+        if (node.right) {
+            recursion(node.right, count + 1);
+        }
+    };
+
+    recursion(root, 1);
+
+    return min;
+};
+
+console.log(
+    "min_depth_of_tree => ",
+    min_depth_of_tree({
+        val: 3,
+        left: {
+            val: 9,
+            left: null,
+            right: null,
+        },
+        right: {
+            val: 20,
+            left: {
+                val: 15,
+                left: null,
+                right: null,
+            },
+            right: {
+                val: 7,
+                left: null,
+                right: null,
+            },
+        },
+    })
+);
+
+//2. Coin Change
+const coin_chnage = (coins, amount) => {
+    let min = -1;
+
+    const memo = {};
+
+    const recursion = (sum, index, count) => {
+        const key = `${sum}-${index}`;
+
+        if (memo[key] !== undefined && memo[key] <= count) return;
+        memo[key] = count;
+
+        if (index >= coins.length || sum > amount) return;
+
+        if (sum === amount) {
+            if (min > count || min === -1) {
+                min = count;
+            }
+            return;
+        }
+
+        recursion(sum + coins[index], index, count + 1);
+        recursion(sum, index + 1, count);
+    };
+
+    recursion(0, 0, 0);
+
+    return min;
+};
+
+console.log("coin_chnage => ", coin_chnage([1, 2, 5], 11));
